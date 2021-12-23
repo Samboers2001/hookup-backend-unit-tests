0ce9b4d8e779e0880d22dbafbf965c6156ebf4d9
Sam Boers <sam@boers.family>
2021-12-23 12:00:49 +0100

    project setup

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace hookup_backend_unit_tetsts
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
